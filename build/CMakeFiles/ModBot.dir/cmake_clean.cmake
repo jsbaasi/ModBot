969150329337dc316f6286de8af537d5138250c2
jsbaasi <jujhaar02@gmail.com>
2025-08-18 19:01:30 +0100

    init

---
+++ build/CMakeFiles/ModBot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModBot.dir/src/main.cpp.o"
+  "CMakeFiles/ModBot.dir/src/main.cpp.o.d"
+  "ModBot"
+  "ModBot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ModBot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ModBot.
+# This may be replaced when dependencies are built.
