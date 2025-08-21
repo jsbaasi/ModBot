64131f8c10f66a2727405f06f1d7e3c147a1c1c1
jsbaasi <jujhaar02@gmail.com>
2025-08-21 23:04:48 +0100

    starting to work on the sqlite stuff

--- src/db.cpp
+++ src/db.cpp
@@ -1,1 +1,10 @@
-#include <db.h>
+#include <sqlite3.h>
+
+// class Database {
+// private:
+//     sqlite3* m_db{};
+// public:
+//     Database() {
+        
+//     }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <dpp/dpp.h>
 #include <string>
+#include <sqlite3.h>
 
 /*
 So the main functionality of the bot is the tracking stuff, need to make a 10sec/1min/5min loop that
@@ -18,6 +19,11 @@ I need to learn about libraries for sqlite
 
 If I don't give it a txt file the bot just hangs, as if waiting for input but nothing happens when
 I type something in
+
+First we need a way to get all registered users, this will mean looking through the main table
+that stores all users and their characteristics
+Then we have either one table storing all balance history or we have table for each person storing
+their balance history, naa one table
 */
 
 int main() {
@@ -39,10 +45,15 @@ int main() {
     bot.on_ready([&bot](const dpp::ready_t& event) {
         
         bot.start_timer([&bot](const dpp::timer& timer){
-            bot.request("https://dpp.dev/DPP-Logo.png", dpp::m_get, [&bot](const dpp::http_request_completion_t& callback) {
-                bot.message_create(dpp::message(1407116920288710726, "").add_file("image.png", callback.body));
-            });
-        }, 10);
+            // bot.request("https://dpp.dev/DPP-Logo.png", dpp::m_get, [&bot](const dpp::http_request_completion_t& callback) {
+            //     bot.message_create(dpp::message(1407116920288710726, "").add_file("image.png", callback.body));
+            // });
+            
+            bot.message_create(dpp::message(1407116920288710726, "hello"));
+
+
+
+        }, 5);
 
         if (dpp::run_once<struct register_bot_commands>()) {
             bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
